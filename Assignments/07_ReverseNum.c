64888d44cb5bab1573383a9b28afbb9c39ad3bd8
sreasgop <sreasgop@gmail.com>
2023-12-26 01:17:13 +0530

    07_ReverseNum.c Modified

--- Assignments/07_ReverseNum.c
+++ Assignments/07_ReverseNum.c
@@ -1,11 +1,10 @@
 // C Program to Reverse a Number
 
 #include<stdio.h>
-#include<math.h>
 
 int main() {
     
-    int num, unit_digit, reverse_num=1, counter=0;
+    int num, unit_digit, reverse_num=0;
     
     printf("\nEnter a number: ");
     scanf("%d", &num);
@@ -13,11 +12,10 @@ int main() {
     while(num){
         unit_digit = num % 10; 
         reverse_num = reverse_num * 10 + unit_digit;
-        counter++;
         num/=10;
     }
 
-    printf("Reversed Number: %lf\n\n", reverse_num/pow(10, counter));
+    printf("Reversed Number: %d\n\n", reverse_num);
 
     return 0;
 }
