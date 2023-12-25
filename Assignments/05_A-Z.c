8194d70b92c351b2cca6f36879be23f2912cbe3e
sreasgop <sreasgop@gmail.com>
2023-12-26 00:35:47 +0530

    05_A-Z.c added

---
+++ Assignments/05_A-Z.c
@@ -0,0 +1,12 @@
+// C Program to Display Character from A to Z Using Loop
+
+#include<stdio.h>
+int main() {
+    int i; 
+    printf("\nLetters in English Alphabet: ");
+    for(i=65; i<91; i++){
+        printf("%c ", i);
+    }
+    printf("\n\n");
+    return 0;
+}
