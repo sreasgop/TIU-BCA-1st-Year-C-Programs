344cf7d69c4e0753e0e5c1b8063477289ae4ff1c
sreasgop <sreasgop@gmail.com>
2023-12-30 06:34:34 +0530

    01_Fibonacci.c improved

--- Assignments/01_Fibonacci.c
+++ Assignments/01_Fibonacci.c
@@ -1,14 +1,16 @@
 // 1. C Program to Display Fibonacci Sequence
+
 #include<stdio.h>
 int main() {
-    int i, n, first_term = 0, second_term = 1, next_term = 0;
+    int i, n;
+    long long int first_term = 0, second_term = 1, next_term = 0;
     
-    printf("Enter the nth term: ");
+    printf("\nEnter the nth term: ");
     scanf("%d", &n);
     
     for (i = 0; i <n; i++)
     {
-        printf("%d ", next_term);
+        printf("%lld ", next_term);
         first_term = second_term; 
         second_term = next_term; 
         next_term = first_term + second_term;
