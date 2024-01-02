b1ebd927ee6d6d711dac6be9bc599f8c498aceca
sreasgop <sreasgop@gmail.com>
2024-01-03 01:01:23 +0530

    31_MatrixAddition.c modifed: Last prinft statement fixed.

--- Assignments/31_MatrixAddition.c
+++ Assignments/31_MatrixAddition.c
@@ -2,23 +2,49 @@
 
 #include <stdio.h>
 int main() {
+    
+    int i, j;
     int arr[3][3]={{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int arr2[3][3] = {{5, 10, 15}, {20, 25, 30}, {35, 40, 45}};
-    int i, j, sum=0;
+    int arrsum[3][3];
+    
 
-    printf("The array is: \n");
+    printf("\n\nThe 1st matrix: \n");
     for ( i = 0; i < 3; i++)
     {
         for ( j = 0; j < 3; j++)
         {
             printf("  %d ", arr[i][j]);
-            sum+=arr[i][j];
         }
         printf("\n");
     }
-    printf("The sum is: %d", sum);
     
-    return 0;
-}
+    printf("\n\n");
+
+    printf("The 2nd matrix: \n");
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("  %d ", arr2[i][j]);
+            arrsum[i][j] = arr[i][j]+arr2[i][j];
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+
+    printf("The Sum of two above Matrix: \n");
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("  %d ", arrsum[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+
     return 0;
 }
